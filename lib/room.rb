1e5df2d01d43afdb8b72c34003cf2f26cd410839
Taylor Phillips <taylorscottphillips@gmail.com>
2020-08-26 10:15:16 -0500

    Add area, paint, and is_painted? methods and tests

--- lib/room.rb
+++ lib/room.rb
@@ -4,6 +4,24 @@ class Room
   def initialize(category, length, width)
     @category = category
     @length = length
-    @width = width
+    @width = width.to_i
+    @is_painted = false
   end
+
+  def area
+    length * width
+  end
+
+  def is_painted?
+    is_painted
+  end
+
+  def paint
+    self.is_painted = true
+  end
+
+  private
+
+  attr_accessor :is_painted
+  attr_reader :length, :width
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -14,10 +14,17 @@ class RoomTest < Minitest::Test
   end
 
   def test_it_can_get_area
-    skip
     room1 = Room.new(:bedroom, 10, '13')
     room2 = Room.new(:living_room, 15, '12')
     assert_equal 130, room1.area
     assert_equal 180, room2.area
+  end
+
+  def test_it_can_be_painted
+    room = Room.new(:bedroom, 10, '13')
+
+    assert_equal false, room.is_painted?
+    room.paint
+    assert_equal true, room.is_painted?
   end
 end
