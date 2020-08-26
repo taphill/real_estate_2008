d719db94a737672c09aaaa2936cccf8d2220aad3
Taylor Phillips <taylorscottphillips@gmail.com>
2020-08-26 10:26:14 -0500

    Add add_rooms method and test to House class

--- lib/house.rb
+++ lib/house.rb
@@ -1,6 +1,17 @@
 class House
+  attr_reader :price, :address, :rooms
+
   def initialize(price, address)
-    @price = price
+    @price = price.sub('$', '').to_i
     @address = address
+    @rooms = []
   end
+
+  def add_room(room)
+    self.rooms << room
+  end
+
+  private
+
+  attr_writer :rooms
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/room'
 require './lib/house'
 
 class HouseTest < Minitest::Test
@@ -7,5 +8,23 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
 
     assert_instance_of House, house
+  end
+
+  def test_it_has_attributes
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal 400_000, house.price
+    assert_equal "123 sugar lane", house.address
+  end
+
+  def test_it_can_add_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+
+    assert_equal [room_1, room_2], house.rooms
   end
 end
