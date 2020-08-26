5d5c14317ead8efbeac0e7744db72768e547e18f
Taylor Phillips <taylorscottphillips@gmail.com>
2020-08-26 10:18:22 -0500

    Add House class and House test

---
+++ test/house_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/house'
+
+class HouseTest < Minitest::Test
+  def test_it_exists
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_instance_of House, house
+  end
+end
